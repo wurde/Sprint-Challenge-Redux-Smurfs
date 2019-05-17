242e6c902714889357d93a0034b93f75f8e29a21
Andy Bettisworth <andy@accreu.com>
2019-05-17 10:34:48 -0500

    Import global styles

--- smurfs/src/App.js
+++ smurfs/src/App.js
@@ -27,10 +27,18 @@ require('./App.scss')
 function App() {
   return (
     <Provider store={store}>
-      <h1>SMURFS! 2.0 W/ Redux</h1>
-      <div>Welcome to your Redux version of Smurfs!</div>
-      <div>Start inside of your `src/index.js` file!</div>
-      <div>Have fun!</div>
+      <main>
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <h1>SMURFS! 2.0 W/ Redux</h1>
+              <div>Welcome to your Redux version of Smurfs!</div>
+              <div>Start inside of your `src/index.js` file!</div>
+              <div>Have fun!</div>
+            </div>
+          </div>
+        </div>
+      </main>
     </Provider>
   )
 }

c5bc83aae2a48ed952d0b1589c5ca7663adee8ae
Andy Bettisworth <andy@accreu.com>
2019-05-17 10:52:40 -0500

    Add resolvers for FETCH and CREATE

--- smurfs/src/store/reducers/index.js
+++ smurfs/src/store/reducers/index.js
@@ -25,6 +25,38 @@ const initialState = {
 
 function reducers(state = initialState, action) {
   switch (action.type) {
+    case actions.FETCH_SMURFS_START:
+      return Object.assign({}, state, {
+        isFetchingSmurfs: true,
+        error: ''
+      })
+    case actions.FETCH_SMURFS_SUCCESS:
+      return Object.assign({}, state, {
+        isFetchingSmurfs: false,
+        error: '',
+        smurfs: action.payload
+      })
+    case actions.FETCH_SMURFS_ERROR:
+      return Object.assign({}, state, {
+        isFetchingSmurfs: false,
+        error: action.payload
+      })
+    case actions.CREATE_SMURF_START:
+      return Object.assign({}, state, {
+        isCreatingSmurf: true,
+        error: ''
+      })
+    case actions.CREATE_SMURF_SUCCESS:
+      return Object.assign({}, state, {
+        isCreatingSmurf: false,
+        error: '',
+        smurfs: action.payload
+      })
+    case actions.CREATE_SMURF_ERROR:
+      return Object.assign({}, state, {
+        isCreatingSmurf: false,
+        error: action.payload
+      })
     default:
       return state
   }
